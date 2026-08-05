210aa29ed2a7e41d5ddc05adfd1c85129bf387ce
Abdul Rehman Rajpoot <ranaabdulrehman1986@gmail.com>
2026-08-06 00:54:04 +0500

    Add files via upload

--- plugins/cat-04-download.js
+++ plugins/cat-04-download.js
@@ -105,7 +105,7 @@ module.exports = {
     return module.exports;
   })();
   _bundle.push(_m);
-  if (_m.command) require('../lib/commandHandler').registerCommand(_m.command, _m);
+  if (_m.command) require('../lib/commandHandler').registerCommand(_m);
 } catch(e) { console.warn('[BUNDLE:cat-04-download] tiktok.js:', e.message); }
 
 /* ===== instagram.js ===== */
@@ -147,7 +147,7 @@ module.exports = {
     return module.exports;
   })();
   _bundle.push(_m);
-  if (_m.command) require('../lib/commandHandler').registerCommand(_m.command, _m);
+  if (_m.command) require('../lib/commandHandler').registerCommand(_m);
 } catch(e) { console.warn('[BUNDLE:cat-04-download] instagram.js:', e.message); }
 
 /* ===== twitter.js ===== */
@@ -180,7 +180,7 @@ module.exports = {
     return module.exports;
   })();
   _bundle.push(_m);
-  if (_m.command) require('../lib/commandHandler').registerCommand(_m.command, _m);
+  if (_m.command) require('../lib/commandHandler').registerCommand(_m);
 } catch(e) { console.warn('[BUNDLE:cat-04-download] twitter.js:', e.message); }
 
 /* ===== facebook.js ===== */
@@ -212,7 +212,7 @@ module.exports = {
     return module.exports;
   })();
   _bundle.push(_m);
-  if (_m.command) require('../lib/commandHandler').registerCommand(_m.command, _m);
+  if (_m.command) require('../lib/commandHandler').registerCommand(_m);
 } catch(e) { console.warn('[BUNDLE:cat-04-download] facebook.js:', e.message); }
 
 module.exports = _bundle;
